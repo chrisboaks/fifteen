b0e5d1c16f007fafe3a0af41ea295a396a30fca2
Christopher Boaks <chris.boaks@gmail.com>
2014-12-04 11:53:22 -0500

    Render a thing without excessive script in HTML

--- scripts/threeScene.js
+++ scripts/threeScene.js
@@ -1,31 +1,16 @@
-// var scene = new THREE.Scene();
-// var camera = new THREE.PerspectiveCamera( 60, window.innerWidth/window.innerHeight, 0.1, 1000 );
-// var directionalLight = new THREE.PointLight( 0x006600, 0.75 );
-// directionalLight.position.set( 2, 2, 6 );
-// scene.add( directionalLight );
-// var light = new THREE.AmbientLight( 0x668866 ); // soft white light
-// scene.add( light );
-//
-// var renderer = new THREE.WebGLRenderer();
-// renderer.setSize( window.innerWidth, window.innerHeight );
-// document.body.appendChild( renderer.domElement );
-
-
 (function () {
-
-  window.Fifteen = window.Fifteen || {};
 
   var scene = Fifteen.scene = new THREE.Scene();
   var camera = new THREE.PerspectiveCamera( 60, 1, 0.1, 1000 );
+  var directionalLight = new THREE.PointLight( 0x006600, 0.75 );
+  directionalLight.position.set( 2, 2, 6 );
+  scene.add( directionalLight );
+  var light = new THREE.AmbientLight( 0x668866 ); // soft white light
+  scene.add( light );
+
   var renderer = new THREE.WebGLRenderer();
-  var $container = $('#container');
-
-  scene.add(camera);
-  camera.position.z = 10;
   renderer.setSize(800, 800);
-
-  $container.append(renderer.domElement);
-  camera.position.z = 20;
+  $('#container').append(renderer.domElement);
 
   var geometry = new THREE.BoxGeometry( 5, 5, 5 );
   var material = new THREE.MeshPhongMaterial( { color: 0x909090} );
@@ -33,22 +18,17 @@
   scene.add( cube );
   console.log(cube);
 
+  camera.position.z = 20;
+
+
   var render = Fifteen.render = function () {
     requestAnimationFrame( render );
-
-    // dodecahedron.rotation.x += 0.025;
-    // dodecahedron.rotation.y += 0.025;
-    //
-    // cylinder.rotation.x += 0.005;
-    // cylinder.rotation.y += 0.025;
-    // cylinder.rotation.z += 0.025;
-    //
-    //
-    // cube.rotation.x += 0.025;
-    // cube.rotation.y += 0.025;
+    cube.rotation.x += 0.025;
+    cube.rotation.y += 0.025;
 
     renderer.render(scene, camera);
   };
 
+  render();
 
 })();
