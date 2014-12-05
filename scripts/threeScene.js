351c3a14c5fbba7ac7d1845db678dcb5bf2e1eda
Christopher Boaks <chris.boaks@gmail.com>
2014-12-05 16:11:11 -0500

    Simplify, add GUI js

--- scripts/threeScene.js
+++ scripts/threeScene.js
@@ -14,29 +14,12 @@
   var light = new THREE.AmbientLight( 0x444444 );
   scene.add( light );
 
-
-  // var geometry = new THREE.BoxGeometry( 5, 5, 1 );
-  // var material = new THREE.MeshPhongMaterial( { color: 0x909090} );
-  // var cube1 = new THREE.Mesh( geometry, material );
-  // scene.add( cube1 );
-  // var cube2 = new THREE.Mesh( geometry, material );
-  // scene.add( cube2 );
-  // cube2.position.x = 5;
-
-
-
-  // camera.position.x = 7.5;
-  // camera.position.y = -7.5;
   camera.position.z = 20;
 
 
   var render = Fifteen.render = function () {
     window.frame.refreshTileCoordinates();
     requestAnimationFrame( render );
-    // cube1.rotation.x += 0.025;
-    // cube1.rotation.y += 0.025;
-    // cube2.rotation.x += 0.025;
-    // cube2.rotation.y += 0.025;
 
     renderer.render(scene, camera);
   };
